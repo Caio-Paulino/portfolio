f495e9dccbd6a843b360f93122a185e91c88edca
Caio Paulino <10paulinhosilva10@gmail.com>
2024-06-25 11:35:30 -0300

    tela de habilidades

---
+++ script.js
@@ -0,0 +1,22 @@
+function clickMenu() {
+    if(itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block' 
+    }
+}
+
+// Scroll section active link
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('ul li a');
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         // if(top >= )
+//     })
+// }
